b8f2a3ee3fee3a983efdb525e2fc487198a2e528
Nizar EL Ouarti <46408023+Nizarel@users.noreply.github.com>
2021-03-18 10:16:38 +0000

    provider

--- WebApp/main.tf
+++ WebApp/main.tf
@@ -1,8 +1,16 @@
-provider "azurerm" {
-  version = "=2.46.0"
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 }
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.resource_group_location
