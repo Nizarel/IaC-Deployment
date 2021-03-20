c8a8ea23942381808e8c2b2f25bd02689d06779f
Nizar EL Ouarti <46408023+Nizarel@users.noreply.github.com>
2021-03-20 07:04:45 +0000

    adding variables

--- vnetfw/jbox.tf
+++ vnetfw/jbox.tf
@@ -47,7 +47,7 @@ resource "azurerm_network_interface_security_group_association" "jbox-rg" {
 }
 
 resource "azurerm_windows_virtual_machine" "jbox-rg" {
-  name                = "jbox-rg-machine"
+  name                = "jbox-vm01"
   resource_group_name = azurerm_resource_group.jbox-rg.name
   location            = azurerm_resource_group.jbox-rg.location
   size                = "Standard_DS1_v2"
---
+++ vnetfw/variables.tf
@@ -0,0 +1,56 @@
+variables  fw-rg-name     {
+    type = string 
+    default = "fw-rg"
+}
+variables  location-name  {
+    type = string 
+    default = "eastus"
+}
+variables  fw-vnet-name   {
+    type = string 
+    default = "fw-vnet"
+}
+variables  jb-sub-name    {
+    type = string 
+    default = "jbox-subnet"
+}
+variables  pip-name       {
+    type = string 
+    default = "pub-ip01"
+}
+variables  fw-name        {
+    type = string 
+    default = "fw-01"
+}
+variables  be-rg-name     {
+    type = string 
+    default = "be-rg"
+}
+variables  web-vnet-name  {
+    type = string 
+    default = "web-vnet"
+}
+variables  web-sub-name   {
+    type = string 
+    default = "web-subnet"
+}
+variables  web-vm-name    {
+    type = string 
+    default = "Web-vm01"
+}
+variables  jb-rg-name     {
+    type = string 
+    default = "jbox-rg"
+}
+variables  jb-vm-name     {
+    type = string 
+    default = "jbox-vm01"
+}
+variables  admin_username {
+    type = string 
+    default = "adminuser"
+}
+variables  admin_password {
+    type = string 
+    default = "P@$$w0rd1234!"
+}
