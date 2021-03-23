6e69f3f6b48302ce596f34a689fdf579b4e8ab83
Nizar EL Ouarti <46408023+Nizarel@users.noreply.github.com>
2021-03-23 14:28:22 +0000

    Set up vnet module

--- Kool-App/main.tf
+++ Kool-App/main.tf
@@ -1,4 +1,4 @@
-/*terraform {
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,39 +6,51 @@
     }
   }
 }
-
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
-*/
+
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+resource "azurerm_resource_group" "kool-rg" {
+  name     = "${var.env}-kool-rg"
+  location = var.location-name
 }
 
-resource "azurerm_api_management" "example" {
-  name                = var.apim_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  publisher_name      = "My Company"
-  publisher_email     = "company@terraform.io"
-
-  sku_name = "Developer_1"
-}
-
-resource "azurerm_api_management_api" "example" {
-  name                = "example-api"
-  resource_group_name = azurerm_resource_group.example.name
-  api_management_name = azurerm_api_management.example.name
-  revision            = "1"
-  display_name        = var.api_name
-  path                = var.api_path
-  protocols           = ["https"]
-
-  import {
-    content_format = "swagger-link-json"
-    content_value  = var.api_url
+module "kool-vnet" {
+  source              = "Azure/vnet/azurerm"
+  vnet_name           = "${var.env}-kool-vnet"
+  resource_group_name = azurerm_resource_group.fe-rg.name
+  address_space       = ["10.0.0.0/23"]
+  subnet_prefixes     = ["10.0.0.0/24", "10.0.1.0/24"]
+  subnet_names        = ["${var.env}-app-subnet", "${var.env}-data-subnet"]
+  tags = {
+    environment = var.env
   }
 }
+
+
+# resource "azurerm_api_management" "example" {
+#   name                = var.apim_name
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   publisher_name      = "My Company"
+#   publisher_email     = "company@terraform.io"
+
+#   sku_name = "Developer_1"
+# }
+
+# resource "azurerm_api_management_api" "example" {
+#   name                = "example-api"
+#   resource_group_name = azurerm_resource_group.example.name
+#   api_management_name = azurerm_api_management.example.name
+#   revision            = "1"
+#   display_name        = var.api_name
+#   path                = var.api_path
+#   protocols           = ["https"]
+
+#   import {
+#     content_format = "swagger-link-json"
+#     content_value  = var.api_url
+#   }
+# }
--- Kool-App/variables.tf
+++ Kool-App/variables.tf
@@ -1,29 +1,29 @@
-variable "resource_group_name" {
-    type        = string
-    description = "RG name in Azure"
+variable env {
+  type    = string
+  description = "Environment Name"
+  default = "test"
+}
+variable location-name {
+  type    = string
+  default = "westeurope"
 }
 
-variable "resource_group_location" {
-    type        = string
-    description = "RG location in Azure"
-}
+# variable "apim_name" {
+#   type        = string
+#   description = "API Management Name"
+#   }
 
-variable "apim_name" {
-  type        = string
-  description = "API Management Name"
-  }
+# variable "api_name" {
+#   type        = string
+#   description = "Default API Name"
+#   }
 
-variable "api_name" {
-  type        = string
-  description = "Default API Name"
-  }
+# variable "api_path" {
+#   type        = string
+#   description = "Default API Path"
+#   }
 
-variable "api_path" {
-  type        = string
-  description = "Default API Path"
-  }
-
-variable "api_url" {
-  type        = string
-  description = "Default API URL"
-  }
+# variable "api_url" {
+#   type        = string
+#   description = "Default API URL"
+#   }
