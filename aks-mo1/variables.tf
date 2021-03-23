910235fe7a25525701ce6a43e5bd32c4abf49c2f
Nizar EL Ouarti <46408023+Nizarel@users.noreply.github.com>
2021-03-23 08:03:12 +0000

    Aks tf

---
+++ aks-mo1/variables.tf
@@ -0,0 +1,12 @@
+variable "aks-rg-name" {
+  type    = string
+  default = "aks-rg"
+}
+variable "location-name" {
+  type    = string
+  default = "eastus"
+}
+variable "aks-vnet-name" {
+  type    = string
+  default = "aks-vnet"
+}
